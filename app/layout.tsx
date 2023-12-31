ceab6e4948b54dd24ae8037ec1b1a309f645fae5
Shamith Udayanga <141323080+shamithudayanga@users.noreply.github.com>
2023-12-31 18:59:54 +0530

    sham

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import './globals.css'
 
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 
 
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="">{children}</body>
       <div>
        
       </div>
